afd133f75e0e2695f4211d7b28a2472d71861ade
PalomoRodriguezHectorLenin <hpalomor2000@alumno.ipn.mx>
2022-11-24 12:44:42 -0600

    noc

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,12 @@
 async function api(){
     
 
-        const urlAPI = 'https://gateway.marvel.com/v1/public/characters?ts=1&apikey=a048e153956b0178033d854e0f9569f6&hash=15722fef819c7253427082372fb36aaf';
+        const urlAPI = 'https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=a048e153956b0178033d854e0f9569f6&hash=15722fef819c7253427082372fb36aaf';
         const container = document.querySelector('#marvel-row');
         let contentHTML = '';
 
         const api = await fetch(urlAPI);
-        const data = await marvel.json();
+        const dato = await api.json();
     
         Object.values(dato.data.results).forEach(hero => {
             let urlHero = hero.urls[0].url;
