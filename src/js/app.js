7bf107c4c7c1f19f51ca7994470167fdaa5de44d
PalomoRodriguezHectorLenin <hpalomor2000@alumno.ipn.mx>
2022-11-24 12:31:44 -0600

    Mmmm

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,22 @@
-const api = {
-    render: () => {
+async function api(){
+    
 
-        const urlAPI = 'https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=a048e153956b0178033d854e0f9569f6&hash=15722fef819c7253427082372fb36aaf';
+        const urlAPI = 'https://gateway.marvel.com/v1/public/characters?ts=1&apikey=a048e153956b0178033d854e0f9569f6&hash=15722fef819c7253427082372fb36aaf';
         const container = document.querySelector('#marvel-row');
         let contentHTML = '';
+
+        const api = await fetch(urlAPI);
+        const data = await marvel.json();
     
-        fetch(urlAPI)
-          .then(res => res.json())
-          .then((json) => {
-            for (const hero of json.data.results) {
-              let urlHero = hero.urls[0].url;
-              contentHTML += `
+        Object.values(dato.data.results).forEach(hero => {
+            let urlHero = hero.urls[0].url;
+            contentHTML += `
                 <div class="col-md-4">
                     <a href="${urlHero}" target="_blank">
                       <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img-thumbnail">
                     </a>
                     <h3 class="title">${hero.name}</h3>
                 </div>`;
-            }
-            container.innerHTML = contentHTML;
-          })
-      }
-    };
-
-api.render();
+        });
+        container.innerHTML = contentHTML;
+    }
